3f8613fb19df0e4c37afe85a07326f25340a52c0
Jeffry Babb <ahuimanu@gmail.com>
2020-08-20 10:07:09 -0500

    fixes from secret key modification

--- blog/blogsite/blogsite/settings.py
+++ blog/blogsite/blogsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/etc/secret_key.txt') as f:
+with open('./secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
