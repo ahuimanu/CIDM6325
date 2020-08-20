6757da3ee0ea475652a60e73b45270f0a03fdc75
Jeffry Babb <ahuimanu@gmail.com>
2020-08-20 08:20:58 -0500

    updated models

--- blog/blogsite/blog/admin.py
+++ blog/blogsite/blog/admin.py
@@ -5,10 +5,10 @@ from . models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug',  'author', 'publish', 'status')
-    list_filter = ('status', 'created', 'publish', 'author')
-    search_fields = ('title', 'body')
+    list_display = ('title', 'slug',  'author', 'publish', 'status',)
+    list_filter = ('status', 'created', 'publish', 'author',)
+    search_fields = ('title', 'body',)
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
-    date_hierarchy = 'publish',
-    ordering = ('status', 'publish')
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish',)
