41f4d69c7af67e5c8a870eadb78e723c2c2bd178
Jeffry Babb <ahuimanu@gmail.com>
2020-08-23 17:52:34 -0500

    completed chapter 1 code

--- blog/blogsite/blog/models.py
+++ blog/blogsite/blog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, 
+                     self).get_queryset()\
+                          .filter(status='published')
+                
+
 class Post(models.Model):
     """
     A model for a Blog Post
@@ -28,6 +37,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft',)
+    objects = models.Manager() 
+    published = PublishedManager()
     
     class Meta:
         # sort descending on publish
@@ -35,4 +46,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', 
+                        args=[self.publish.year,
+                              self.publish.month,
+                              self.publish.day,
+                              self.slug])
+    
 
