1eb8c22452519e733089f21667cb9d1bed666c90
Jeffry Babb <ahuimanu@gmail.com>
2020-09-28 16:25:29 -0500

    example queries for T-100

--- t100data/t100data/air_carrier_market_data/urls.py
+++ t100data/t100data/air_carrier_market_data/urls.py
@@ -1,10 +1,26 @@
 # urls.py
 from django.urls import path
 from . views import MarketDataList, \
-                    Top5AirportsPaxByOrigin
-                    
+                    Top5AirportsPaxByOrigin, \
+                    Top5AirportsPaxByDestination, \
+                    TopDistanceByMonth         
+
 
 urlpatterns = [
     path('list/', MarketDataList.as_view(), name="list"),
-    path('top5paxorigin/', Top5AirportsPaxByOrigin.as_view(), name="top5paxorigin"),
+    path('top5paxorigin/', 
+        Top5AirportsPaxByOrigin.as_view(
+            extra_context={'title': "Top 5 Airports - Passengers by Origin Airport"}
+        ),
+        name="top5paxorigin"),
+    path('top5paxdestination/',  
+        Top5AirportsPaxByDestination.as_view(
+            extra_context={'title': "Top 5 Airports - Passengers by Destination Airport"}
+        ), 
+        name="top5paxdestination"),
+    path('topdistance_month/',  
+        TopDistanceByMonth.as_view(
+            extra_context={'title': "Top Distance by Month"}
+        ), 
+        name="topdistance_month"),
 ]
--- t100data/t100data/air_carrier_market_data/views.py
+++ t100data/t100data/air_carrier_market_data/views.py
@@ -1,10 +1,36 @@
 # Create your views here.
 from django.views.generic import ListView
+from django.db.models import Max, Sum
+
 from . models import MarketData
 
 class MarketDataList(ListView):
     model = MarketData
 
+# What are the top 5 airports in terms of: Total passengers by origin
 class Top5AirportsPaxByOrigin(ListView):
-    model = MarketData
-    template_name="Top5AirportsPaxByOrigin_list.html"
+    context_object_name = "airport_list"
+    queryset = MarketData.objects.values('orig_iata_code','orig_city_name') \
+                                 .annotate(total_pax=Sum('passengers')) \
+                                 .order_by('-total_pax')[0:5]
+    template_name="rankorder_list_origin.html"
+
+# What are the top 5 airports in terms of: Total passengers by destination
+class Top5AirportsPaxByDestination(ListView):
+    context_object_name = "airport_list"
+    queryset = MarketData.objects.values('dest_iata_code','dest_city_name') \
+                                 .annotate(total_pax=Sum('passengers')) \
+                                 .order_by('-total_pax')[0:5]
+    template_name="rankorder_list_destination.html"
+
+
+# Which airport reported the longest distance by month?
+class TopDistanceByMonth(ListView):
+    context_object_name = "airport_list"
+    queryset = MarketData.objects.filter() \
+                                 .values('orig_iata_code','orig_city_name','dest_iata_code','dest_city_name') \
+                                 .annotate(total_distance=Max('distance')) \
+                                 .order_by('-total_distance')[0:5]
+#                                .order_by('month')[0:5]
+
+    template_name="rankorder_list_origin_distance_month.html"
