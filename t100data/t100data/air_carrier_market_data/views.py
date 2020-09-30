9940c0a03f32a6cd94c07aee25ded6c49556fd4e
Jeffry Babb <ahuimanu@gmail.com>
2020-09-29 23:30:34 -0500

    updates to t100

--- t100data/t100data/air_carrier_market_data/views.py
+++ t100data/t100data/air_carrier_market_data/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+import pdb
 from django.views.generic import ListView
 from django.db.models import Max, Sum
 
@@ -10,9 +11,10 @@ class MarketDataList(ListView):
 # What are the top 5 airports in terms of: Total passengers by origin
 class Top5AirportsPaxByOrigin(ListView):
     context_object_name = "airport_list"
-    queryset = MarketData.objects.values('orig_iata_code','orig_city_name') \
-                                 .annotate(total_pax=Sum('passengers')) \
-                                 .order_by('-total_pax')[0:5]
+    queryset = MarketData.objects \
+                        .values('orig_iata_code','orig_city_name') \
+                        .annotate(total_pax=Sum('passengers')) \
+                        .order_by('-total_pax')[0:5]
     template_name="rankorder_list_origin.html"
 
 # What are the top 5 airports in terms of: Total passengers by destination
@@ -27,10 +29,31 @@ class Top5AirportsPaxByDestination(ListView):
 # Which airport reported the longest distance by month?
 class TopDistanceByMonth(ListView):
     context_object_name = "airport_list"
-    queryset = MarketData.objects.filter() \
-                                 .values('orig_iata_code','orig_city_name','dest_iata_code','dest_city_name') \
-                                 .annotate(total_distance=Max('distance')) \
-                                 .order_by('-total_distance')[0:5]
-#                                .order_by('month')[0:5]
-
     template_name="rankorder_list_origin_distance_month.html"
+
+    def get_queryset(self):
+
+        month_list = []
+
+        # pdb.set_trace()
+
+        # there are six months worth of data
+        # not good ultimately as this is a "hard-coded" fore-knowledge of the data
+        for month in range(1,7):
+            queryset = MarketData.objects \
+                .values('orig_iata_code',
+                        'orig_city_name',
+                        'dest_iata_code',
+                        'dest_city_name',
+                        'month') \
+                .filter(month__exact=month) \
+                .annotate(total_distance=Max('distance')) \
+                .order_by('-total_distance')[0:1]
+            
+            # off by one error for assignment
+
+            month_list.append(queryset)
+
+        # return list
+        return month_list
+
