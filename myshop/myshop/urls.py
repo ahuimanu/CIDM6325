17eb60add09c4a100e5733fbeb81336d6ead81f4
Jeffry Babb <ahuimanu@gmail.com>
2020-10-04 23:58:33 -0500

    started the shop from Chapter 7

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,4 +1,4 @@
-"""bookmarks URL Configuration
+"""myshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,16 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
-    path('social-auth/', include('social_django.urls', namespace='social')),
-    path('images/', include('images.urls', namespace='images')),    
+    path('', include('shop.urls', namespace='shop')),
 ]
 
 if settings.DEBUG:
