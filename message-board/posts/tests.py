e0600a4447a55af735b2d70aef80e04271d2e9bf
Jeffry Babb <ahuimanu@gmail.com>
2022-09-05 16:21:45 -0500

    updated for tests

--- message-board/posts/tests.py
+++ message-board/posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This was created from a test")
+    
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This was created from a test")
