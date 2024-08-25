14580d2d69641e8c36fd108c68e73767c2e16db2
Jeffry Babb <ahuimanu@gmail.com>
2024-08-25 15:57:32 -0500

    updated for mele chapter 1

---
+++ blogsite/blog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Post
+
+
+# Register your models here.
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
+    # Per Mele: Django 5.0 introduces facet filters to the administration site, showcasing facet counts.
+    # These counts indicate the number of objects corresponding to each specific filter,
+    # making it easier to identify matching objects in the admin changelist view.
+    # https://docs.djangoproject.com/en/5.1/releases/5.0/#facet-filters-in-the-admin
+    # https://docs.djangoproject.com/en/5.1/ref/contrib/admin/filters/#facet-filters
+    show_facets = admin.ShowFacets.ALWAYS
---
+++ blogsite/blog/models.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return self.title
