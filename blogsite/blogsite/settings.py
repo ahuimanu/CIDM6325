831660e140f201a1ed16a0ea62b25673e92f5d05
Jeffry Babb <ahuimanu@gmail.com>
2024-09-29 12:01:13 -0500

    updated for postgres issues

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -87,11 +87,20 @@ WSGI_APPLICATION = "blogsite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# note, althought not the best approach, it is simple to switch bewteen sqlite and postgresql
+# by commenting/uncommenting the relevant lines below
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+    }    
 }
 
 
