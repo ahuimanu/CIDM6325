f784e1fbdcc9664b00e520239c9f0e999888a749
Jeffry Babb <ahuimanu@gmail.com>
2024-09-29 08:31:00 -0500

    classy class-based views

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
@@ -9,8 +9,20 @@ def post_list(request):
     post_list = Post.published.all()
     # using the Django paginator - takes the list of objects and the number of objects per page
     paginator = Paginator(post_list, 5)  # 5 posts in each page
+
+    # test for and handle page out of bounds errors
     page_number = request.GET.get("page", 1)
-    posts = paginator.get_page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    # what if the page requested is not a number?
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    # what if the page requested is out of range?
+    except EmptyPage:
+        # If page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
