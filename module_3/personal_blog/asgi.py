8ba17e1a97c9fe96ba1ab5c4efd3b682eff02ce8
Alexander Lawson <Alexander.lawson@clevelandcliffs.com>
2025-10-10 03:01:45 -0500

    M3 base

--- module_3/personal_blog/asgi.py
+++ module_3/personal_blog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for module_3_blog project.
+ASGI config for personal_blog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'module_3_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_blog.settings')
 
 application = get_asgi_application()
--- module_3/personal_blog/wsgi.py
+++ module_3/personal_blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for module_3_blog project.
+WSGI config for personal_blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'module_3_blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_blog.settings')
 
 application = get_wsgi_application()
