62effe094e82b17b8b769346386e5e7e450e1dc5
alelawsoWTAMU <alawson1@buffs.wtamu.edu>
2025-10-15 18:46:40 -0500

    Delete module_3 directory

--- module_3/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_details_view, name='home_details'),  # Set as default view
-    path('home-details/', views.home_details_view, name='home_details'),
-    path('home-details-list/', views.home_details_list_view, name='home_details_list'),
-]
