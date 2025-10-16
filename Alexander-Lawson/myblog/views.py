3bd4e1e96ef5133fc35ee4710db4790fa0088fa4
alelawsoWTAMU <alawson1@buffs.wtamu.edu>
2025-10-15 20:07:29 -0500

    Delete Alexander-Lawson directory

--- Alexander-Lawson/myblog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import BlogPost
-
-def blog_list(request):
-    posts = BlogPost.objects.all().order_by("-published_date")  # Double quotes used here
-    return render(request, "myblog/blog_list.html", {"posts": posts})  # Double quotes used here
