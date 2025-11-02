e886b8a5346f09c5e8765f22fb2915dac78da2ab
Jeffry Babb <ahuimanu@gmail.com>
2025-11-02 16:38:08 -0600

    feat: wire domain apps into settings and add project templates dir (ADR-1.0.0-02)
    Add apps.calculators, apps.airports, apps.accounts, apps.trips, apps.search, apps.core to INSTALLED_APPS. Set TEMPLATES DIRS to include project-level templates/. Verified with manage.py check.
    
    Refs #21

--- travelmathlite/core/settings.py
+++ travelmathlite/core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Domain apps
+    "apps.calculators",
+    "apps.airports",
+    "apps.accounts",
+    "apps.trips",
+    "apps.search",
+    "apps.core",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
